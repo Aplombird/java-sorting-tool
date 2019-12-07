6ee688ed265317fd6f66b31c712017f37e4b96e8
Yongchao Lyu <Yongchao.Lyu@anu.edu.au>
2019-12-07 20:20:14 +1100

    Add class DeepCompare, which implements the Comparator interface, for developing core sorting algorithm.

---
+++ src/diversity/arrays/DeepCompare.java
@@ -0,0 +1,10 @@
+package diversity.arrays;
+
+import java.util.Comparator;
+
+public class DeepCompare implements Comparator<DataHolder> {
+    @Override
+    public int compare(DataHolder o1, DataHolder o2) {
+        return 0;
+    }
+}
