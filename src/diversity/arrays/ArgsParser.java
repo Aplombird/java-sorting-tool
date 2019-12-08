fc9a8d03eab0162c13a4ff7cb7946986f5569b96
Yongchao Lyu <Yongchao.Lyu@anu.edu.au>
2019-12-08 20:05:03 +1100

    Finish the logic of argument parsing, which only support fields names to be sorted in this version.

--- src/diversity/arrays/ArgsParser.java
+++ src/diversity/arrays/ArgsParser.java
@@ -1,5 +1,11 @@
 package diversity.arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 /**
  * The object of this class is used for parsing and holding arguments
  * that read from the command line at the very beginning of the program
@@ -8,4 +14,26 @@ package diversity.arrays;
  * @version 1.0
  */
 class ArgsParser {
+    Map<String, List<String>> arguments = new HashMap<>();
+    private static Logger logger = ToolLogger.getInstance();
+
+    ArgsParser(String[] args) {
+        logger.info("Parsing inputted command-line arguments ...");
+
+        if (args.length == 0) {
+            logger.severe("No arguments are given for sorting!");
+            System.exit(0);
+        }
+        this.arguments.put("object", Arrays.asList(args));
+    }
+
+    boolean areSortedFieldsLegal(List<String> legalHeads) {
+        for (String obj: this.arguments.get("object")) {
+            if (!legalHeads.contains(obj)) {
+                logger.severe(String.format("No field named \"%s\" can be used for sorting!", obj));
+                return false;
+            }
+        }
+        return true;
+    }
 }
